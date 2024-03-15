4b26e35a4bb926e815ed03ea546eb5a74f866dec
Sam241632 <163418353+Sam241632@users.noreply.github.com>
2024-03-15 10:06:59 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function mudarimagemfora()
+{
+    document.getElementById("imagem").src ="https://i.kym-cdn.com/photos/images/newsfeed/001/635/951/22d.jpeg"
+}
+function mudarimagemdentro()
+{
+    document.getElementById("imagem").src ="https://imageproxyb.ifunny.co/crop:x-20,resize:640x,quality:90x75/images/17b28bb4263af1aaceb06329d302c5e9d83833221af2125f725d6c4ca1209593_1.jpg"
+}
